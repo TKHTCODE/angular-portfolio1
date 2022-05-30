95c7d493f1818c9ad4fcb0046914a1d63639dce8
TKHTS <sakuranohanabira1269@gmail.com>
2022-05-29 18:42:04 -0700

    Delete test data

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,13 +8,6 @@ import { User } from './class/user';
 const CURRENT_USER: User = new User(1, 'Taka');
 const ANOTHER_USER: User = new User(2, 'John');
 
-const COMMENTS: Comment[] = [
-  new Comment(ANOTHER_USER, 'Hey'),
-  new Comment(ANOTHER_USER, 'How are you doing?'),
-  new Comment(CURRENT_USER, 'Yeah,'),
-  new Comment(CURRENT_USER, 'I am doing good.')
-];
-
 @Component({
   selector: 'ac-root',
   templateUrl: './app.component.html',
