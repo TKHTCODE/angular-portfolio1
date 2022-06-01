798a39ab3adc8be959cfd6814f122ec06d74defe
TKHTS <sakuranohanabira1269@gmail.com>
2022-06-01 10:23:12 -0700

    Add routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,17 @@
+import { RouterModule, Routes } from '@angular/router';
 import { NgModule } from '@angular/core';
+import { ChatComponent } from './chat/chat.component';
 
+const routes: Routes = [
+  { path: '', component: ChatComponent }
+]
 
 @NgModule({
-  declarations: [],
   imports: [
-    CommonModule
+    RouterModule.forRoot(routes)
+  ],
+  exports: [
+    RouterModule
   ]
 })
 export class AppRoutingModule { }
