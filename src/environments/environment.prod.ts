e37094e0d49d80c411c47b34883227b54550ad4b
TKHTS <sakuranohanabira1269@gmail.com>
2022-06-02 13:27:08 -0700

    Fix setting for deploy

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyAFMVI3sQ4LpIQMPdHPiaJ9CDORZHBjCtQ",
+    authDomain: "angular-typescript-afa57.firebaseapp.com",
+    databaseURL: "https://angular-typescript-afa57-default-rtdb.firebaseio.com",
+    projectId: "angular-typescript-afa57",
+    storageBucket: "angular-typescript-afa57.appspot.com",
+    messagingSenderId: "176994410566",
+    appId: "1:176994410566:web:4df9f6a0ae1c49f4b32a47"
+  }
 };
